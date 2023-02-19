e190f1952f15f5733c5da911f4acb0fdab52693e
_Dan1la <goldphoenixoriginal@mail.ru>
2023-02-19 20:14:04 +0300

    Refactor with ChatGPT

--- classes/color.py
+++ classes/color.py
@@ -10,11 +10,11 @@ class Color:
         """ Sets the class colors
         :param color: int tuple (r, g, b) / (r, g, b, a) """
         if len(color) == 3:  # if there is no alpha channel
-            r, g, b = color[0], color[1], color[2]
+            r, g, b = color
             r, g, b = self.fit(r), self.fit(g), self.fit(b)
-            self.r, self.g, self.b = r, g, b
+            self.r, self.g, self.b, self.a = r, g, b, 255
         elif len(color) == 4:  # if alpha channel is present
-            r, g, b, a = color[0], color[1], color[2], color[3]
+            r, g, b, a = color
             r, g, b, a = self.fit(r), self.fit(g), self.fit(b), self.fit(a)
             self.r, self.g, self.b, self.a = r, g, b, a
 
@@ -29,27 +29,17 @@ class Color:
     def get_hex_rgb(self):
         """ Converts red green and blue values to hex string
         :return: Hex color string """
-        r = hex(self.r)[2:].zfill(2)
-        g = hex(self.g)[2:].zfill(2)
-        b = hex(self.b)[2:].zfill(2)
-        return f'{r}{g}{b}'
+        r, g, b = self.get_rgb()
+        return f'{r:02x}{g:02x}{b:02x}'
 
     def get_hex_rgba(self):
         """ Converts red green and blue values to hex string with alpha channel
         :return: Hex color string """
-        r = hex(self.r)[2:].zfill(2)
-        g = hex(self.g)[2:].zfill(2)
-        b = hex(self.b)[2:].zfill(2)
-        a = hex(self.a)[2:].zfill(2)
-        return f'{r}{g}{b}{a}'
+        r, g, b, a = self.get_rgba()
+        return f'{r:02x}{g:02x}{b:02x}{a:02x}'
 
     @staticmethod
     def fit(x):
         """ Fits color value to [0; 255] interval in case if function got incorrect color parameter
         :return: Normalized color value """
-        if x < 0:
-            return 0
-        elif x > 255:
-            return 255
-        else:
-            return x
+        return max(0, min(255, x))
