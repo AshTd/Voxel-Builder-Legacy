7d4d801d4becd5978ad21d3288f236cfc834c60d
_Dan1la <goldphoenixoriginal@mail.ru>
2023-02-20 14:37:01 +0300

    Add comments to main.py

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from app import VoxelBuilder
 
 
 def main():
-    app = VoxelBuilder()
-    app.run()
+    """ Initializes and runs the VoxelBuilder application. """
+    try:
+        # Initialize the VoxelBuilder application
+        app = VoxelBuilder()
+        # Run the application
+        app.run()
+        print('Done!')
+
+    except Exception as err:
+        # Handling exceptions
+        print(f'Error occured while running program:')
+        print(f'{err}')
+        exit(1)
 
 
 if __name__ == '__main__':
-    print('Have _a nice day!')
+    # Run the application
+    print('Starting VoxelBuilder application...')
     main()
